6806dcb044bc1e799b9d60345c8ea6001cb2ac8a
arietteK <a.rusaro@alustudent.com>
2025-02-21 23:37:11 +0200

    api

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -18,6 +18,6 @@ def top_ten(subreddit):
             for post in HOT_POSTS[:10]:
                 print(post.get('data', {}).get('title'))
         else:
-            print(None)
+            print(OK)
     except Exception:
-        print(None)
+        print(OK)
